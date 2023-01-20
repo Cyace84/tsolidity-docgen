f904308008232419507b2114d035388ad2704478
cyace84 <cyace84@gmail.com>
2023-01-20 05:02:36 +0100

    removed unused imports

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import path from 'path';
 import { promises as fs } from 'fs';
 import { render } from './render';
 import { Build, buildSite } from './site';
-import { ensureArray } from './utils/ensure-array';
 import { Config, defaults } from './config';
 import { loadTemplates } from './templates';
 
@@ -11,10 +10,17 @@ import { loadTemplates } from './templates';
  * function builds the site and renders it, writing all pages to the output
  * directory.
  */
-export async function main(builds: Build[], userConfig?: Config): Promise<void> {
+export async function main(
+  builds: Build[],
+  userConfig?: Config,
+): Promise<void> {
   const config = { ...defaults, ...userConfig };
 
-  const templates = await loadTemplates(config.theme, config.root, config.templates);
+  const templates = await loadTemplates(
+    config.theme,
+    config.root,
+    config.templates,
+  );
   const site = buildSite(builds, config, templates.properties ?? {});
   const renderedSite = render(site, templates, config.collapseNewlines);
 
