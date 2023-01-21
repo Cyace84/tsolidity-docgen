a187da7c5b8e89f7df4493991a08e388350381ff
cyace84 <cyace84@gmail.com>
2023-01-21 02:48:31 +0100

    added compile ast helpers

---
+++ src/ast-builder/compile-ast.ts
@@ -0,0 +1,40 @@
+import { execSync } from 'child_process';
+import { Config } from '../config';
+import { getAstsFromSources, getContractsList } from './getters';
+
+/**
+ * It takes the config object, gets a list of contracts, and then compiles the AST for each contract
+ * @param {Config} config - The configuration object that we created earlier.
+ */
+export const compileAst = async (config: Config) => {
+  const contracts = getContractsList(config.sourcesDir!);
+  contracts.forEach(contract => {
+    execSync(
+      `${config.compilerPath} --ast-compact-json $PWD/${config.sourcesDir}/${contract} --output-dir=$PWD/${config.astOutputDir}`,
+    );
+  });
+  compileExternalAst(config);
+};
+
+/**
+ * It compiles all the external sources in the project, and saves the ASTs in the `astOutputDir`
+ * directory
+ * @param {Config} config - Config
+ */
+export const compileExternalAst = async (config: Config) => {
+  const { fullSources } = getAstsFromSources(
+    config.astOutputDir!,
+    config.root!,
+  );
+
+  Object.values(fullSources).forEach(source => {
+    for (const ast of source.asts) {
+      const absolutePath = ast.absolutePath;
+      if (!absolutePath.startsWith(config.sourcesDir!)) {
+        execSync(
+          `${config.compilerPath} --ast-compact-json $PWD/${absolutePath} --output-dir=$PWD/${config.astOutputDir}`,
+        );
+      }
+    }
+  });
+};
