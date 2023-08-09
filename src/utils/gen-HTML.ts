f83ef2035aef4e3e9b6b8debf0248896fde09f3f
Javad Yakuzaa <javadyakuzaa@gmail.com>
2023-08-09 16:40:47 +0330

    genHTMLUpdated

--- src/utils/gen-HTML.ts
+++ src/utils/gen-HTML.ts
@@ -1,32 +1,34 @@
 import Processor from "asciidoctor";
 import fs from "fs";
-import path from "path";
+import { extname, resolve } from "path";
 import { Config } from "../config";
 const processor = Processor();
 
 export async function generateHTMLFiles(config: Config): Promise<void> {
   try {
-    const distFolderPath = path.join(config.outputDir!, "dist");
-    if (!fs.existsSync(config.outputDir!)) {
-      throw new Error("No adoc files found!!");
+    const distDirPath = resolve(config.root!, config.outputDir!, "dist");
+    const outputDirPath = resolve(config.root!, config.outputDir!);
+
+    if (!fs.existsSync(outputDirPath)) {
+      throw new Error("No adoc files generated!!");
     }
-    if (!fs.existsSync(distFolderPath)) {
-      fs.mkdirSync(distFolderPath);
+    if (!fs.existsSync(distDirPath)) {
+      fs.mkdirSync(distDirPath);
     } else {
-      const files = fs.readdirSync(distFolderPath);
+      const files = fs.readdirSync(distDirPath);
       for (const file of files) {
-        await fs.promises.unlink(path.join(distFolderPath, file));
+        await fs.promises.unlink(resolve(distDirPath, file));
       }
     }
-    const filesInPath = await fs.promises.readdir(config.outputDir!);
+    const filesInPath = await fs.promises.readdir(outputDirPath);
     const adocFiles = filesInPath.filter((file) => file.endsWith(".adoc"));
     if (adocFiles.length === 0) {
       throw new Error("No adoc files found!!");
     }
     for (const file of filesInPath) {
-      if (path.extname(file) === ".adoc") {
-        await processor.convertFile(path.join(config.outputDir!, file), {
-          to_dir: distFolderPath,
+      if (extname(file) === ".adoc") {
+        await processor.convertFile(resolve(outputDirPath, file), {
+          to_dir: distDirPath,
         });
       }
     }
